6ceea33a4ec704e40b78afafc6fd188f6613838d
ddophi98 <72330884+ddophi98@users.noreply.github.com>
2022-01-27 15:48:47 +0900

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello~~~'
+
+@app.route("/test")
+def finance():
+    a = 3
+    b = 5
+    rst = a + b
+    return render_template('main.html', result=rst)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000,debug=True)
