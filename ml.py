6db2cbe54db833f37f19708998761ed829bad10a
ddophi98 <72330884+ddophi98@users.noreply.github.com>
2022-02-05 21:51:42 +0900

    SRGAN 모델 추가

--- ml.py
+++ ml.py
@@ -1,4 +1,41 @@
-img_path = "./static/uploaded_files/"
+# import numpy as np
+# import tensorflow as tf
+# import cv2
+# import os
+
+uploaded_img_path = "./static/uploaded_files/"
+processed_img_path = "./static/processed_files/"
+# srgan = tf.keras.models.load_model('./model/srgan_G.h5')
+
+
+def interpolation(image):
+    bicubic_lr = cv2.resize(
+        image,
+        dsize=(image.shape[1]//4, image.shape[0]//4), # 저해상도 이미지 크기로 설정
+        interpolation=cv2.INTER_CUBIC # bicubic 설정
+    )
+    return bicubic_lr
+
+def apply_srgan(image):
+    image = tf.cast(image[np.newaxis, ...], tf.float32)
+    sr = srgan.predict(image)
+    sr = tf.clip_by_value(sr, o, 255)
+    sr = tf.round(sr)
+    sr = tf.cast(sr, tf.unit8)
+    return np.array(sr)[0]
+
 
 def imageProcess():
-    print("Wow")
+    print("wow")
+    # #작업하기 전 폴더 정리하기
+    # for f in os.scandir(processed_img_path):
+    #     os.remove(f.path)
+    #
+    # #SRGAN 적용하기
+    # file_list = os.listdir(uploaded_img_path)
+    # for f in file_list:
+    #     img = cv2.imread(uploaded_img_path + f)
+    #     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #     img = interpolation(img)
+    #     sr_img = apply_srgan(img)
+    #     sr_img.save(processed_img_path + f)
